5cb59f3760090b76a93697233e3defc2a8115440
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2025-11-20 19:48:43 +0100

    Add verification structure to the project structure

---
+++ src/main/java/com/collinsceleb/campaign_management/modules/verification/controller/VerificationController.java
@@ -0,0 +1,5 @@
+package com.collinsceleb.campaign_management.modules.verification.controller;
+
+public class VerificationController {
+    
+}
---
+++ src/main/java/com/collinsceleb/campaign_management/modules/verification/entity/VerificationEntity.java
@@ -0,0 +1,5 @@
+package com.collinsceleb.campaign_management.modules.verification.entity;
+
+public class VerificationEntity {
+    
+}
