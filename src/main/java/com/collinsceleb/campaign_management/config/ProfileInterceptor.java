2679a5f355faa8741f8b92da8b66e0f77d7cfec8
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2025-11-22 20:48:43 +0100

    Add middleware for updated profile

---
+++ src/main/java/com/collinsceleb/campaign_management/config/ProfileInterceptor.java
@@ -0,0 +1,34 @@
+package com.collinsceleb.campaign_management.config;
+
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.collinsceleb.campaign_management.modules.user.entity.UserEntity;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class ProfileInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull Object handler) throws Exception {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserEntity user) {
+            if (user.getFirstName() == null || user.getLastName() == null) {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.setContentType("application/json");
+                response.getWriter().write("""
+                            {
+                                "message": "Please update your profile before accessing this resource."
+                            }
+                        """);
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/com/collinsceleb/campaign_management/config/WebConfig.java
+++ src/main/java/com/collinsceleb/campaign_management/config/WebConfig.java
@@ -1,5 +1,24 @@
 package com.collinsceleb.campaign_management.config;
 
-public class WebConfig {
+import org.springframework.lang.NonNull;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    @NonNull
+    private final ProfileInterceptor profileInterceptor;
+
+    @Override
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(profileInterceptor).addPathPatterns(
+                "/api/v1/campaigns/**",
+                "/api/v1/locations/**",
+                "/api/v1/status/**");
+    }
 }
