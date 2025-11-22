ee70e6d02efd549ee4a8467cf5cb64f6c235eb5c
Kolawole Meshack Afuye <collinsceleb@gmail.com>
2025-11-22 20:13:50 +0100

    Add enum for campaign status

--- src/main/java/com/collinsceleb/campaign_management/common/enums/CampaignStatusEnum.java
+++ src/main/java/com/collinsceleb/campaign_management/common/enums/CampaignStatusEnum.java
@@ -1,5 +1,13 @@
 package com.collinsceleb.campaign_management.common.enums;
 
 public enum CampaignStatusEnum {
-    
+    Draft,
+    Active,
+    Inactive,
+    Completed,
+    Running,
+    Paid,
+    Pending,
+    Failed,
+    Success,
 }
